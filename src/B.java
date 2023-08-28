316b06a8de3eb6756a3e8a9d737ae1daf6068e89
AlpDereli <alpdereli26@gmail.com>
2023-08-28 16:27:44 +0300

    Initial commit

---
+++ src/B.java
@@ -0,0 +1,26 @@
+public class B {
+    public static void main(String[] args) {
+        String[][] b = new String[5][3];
+        for (int i = 0; i <b.length;i++)
+        {
+            for (int j = 0;j<b[i].length;j++)
+            {
+                if (i==0||i==2||i == 4)
+                {
+                    b[i][j] ="*";
+                } else if (j==0||j==3) {
+                    b[i][j]="*";
+                }
+                else {
+                    b[i][j]=" ";
+                }
+            }
+        }
+        for (String[] row : b){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
